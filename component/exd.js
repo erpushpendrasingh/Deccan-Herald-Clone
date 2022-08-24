42fde4d10650bb402741730a4d0229bab862af7c
Pushpendra Singh <erpushpendrasingh8900@gmail.com>
2022-08-24 21:01:31 +0530

    day_2 navbar footer

---
+++ component/exd.js
@@ -0,0 +1,9 @@
+const hello = () => {
+     return `<div class="div-1">div1</div>
+        <div class="div-2">div2</div>
+        <div class="div-3">div3</div>
+        <div class="div-4">div4</div>
+        <div class="div-5">div5</div>`;
+};
+
+export { hello };
